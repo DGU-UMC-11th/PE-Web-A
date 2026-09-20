438609b0de1aea2846b4a7f5ebc1acf7b78fe5e6
jiyeon <jiyeon.jeon060220@gmail.com>
2026-09-20 23:20:49 +0900

    week1:mission1:스터디 회언 관리 프로그램

---
+++ jiyeon/week1/src/index.ts
@@ -0,0 +1,32 @@
+type Role = "Plan" | "Web";
+type Member = {
+    id : number;
+    name : string;
+    role : Role;
+    githubId? : string;
+};
+const members : Member[] = [
+    {
+        id : 1,
+        name : "수나",
+        role : "Plan",
+    }, 
+    {
+        id : 2,
+        name : "지연",
+        role : "Web",
+        githubId : "jiyeon0220",
+    },
+];
+function profile(id : number){
+    const foundMember = members.find((member) => member.id == id);
+    if(!foundMember){
+        return "존재하지 않는 회원입니다.";
+    }
+    const githubID = foundMember.githubId ?? "등록되지 않음";
+    return foundMember.name + "/" + foundMember.role + "/" + githubID;
+}
+
+console.log(profile(1));
+console.log(profile(2));
+console.log(profile(999));
